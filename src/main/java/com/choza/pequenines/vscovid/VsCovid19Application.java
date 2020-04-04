26e9fb8673f0d43af70831a2249282c9942e2c19
kevinmont <kevinmontdev@gmail.com>
2020-04-04 01:04:01 -0600

    initial commit

---
+++ src/main/java/com/choza/pequenines/vscovid/VsCovid19Application.java
@@ -0,0 +1,13 @@
+package com.choza.pequenines.vscovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsCovid19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/choza/pequenines/vscovid/VsCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choza.pequenines.vscovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsCovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
